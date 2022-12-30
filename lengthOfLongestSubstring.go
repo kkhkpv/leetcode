f42751d987e6b76c6e28db84002f22075b051980
K Khutorenko <k.khutorenko@gmail.com>
2022-12-30 15:50:58 +0300

    4

---
+++ lengthOfLongestSubstring.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	test := "pwwkew"
+	fmt.Print(lengthOfLongestSubstring(test))
+}
+
+func lengthOfLongestSubstring(s string) int {
+	sRuneSlice := []rune(s)
+	if len(sRuneSlice) <= 1 {
+		return len(sRuneSlice)
+	}
+	left, right, res := 0, 0, 0
+	control := ""
+	for right < len(sRuneSlice) {
+		if !strings.ContainsRune(control, sRuneSlice[right]) {
+			control += string(sRuneSlice[right])
+			right++
+			res = int(math.Max(float64(res), float64(len([]rune(control)))))
+		} else {
+			control = strings.Replace(control, string(sRuneSlice[left]), "", 1)
+			left++
+		}
+	}
+	return res
+}
